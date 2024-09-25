aeffb19e2906277bce0a35739aebfdbe6c731cce
FI281001 <pctrungmagic@gmail.com>
2024-09-25 15:48:54 +0700

    update fix

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-
-// Định nghĩa một Cloud Function
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
