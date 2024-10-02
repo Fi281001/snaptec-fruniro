f360d49edcafa146aa560b625acb14cce530d58f
nguyenvanron210 <nguyenvanron210@gmail.com>
2024-10-02 15:47:33 +0700

    fix-css-footer

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -134,16 +134,17 @@ export default function Checkout() {
     <div>
       <Rectangle title="Check out" />
       <form ref={form} onSubmit={sendEmail}>
-        {" "}
         {/* Sử dụng ref ở đây */}
         <div className="container-contact2">
           <h1>Check Out</h1>
           <div className="container-contact__group2">
             <div className="container-contact__group-right2">
-              <span>Name: {mail.split("@")[0]}</span>
-              <span>Email Address: {user ? user.email : "N/A"}</span>
+              <span className="display-flex">Name: {mail.split("@")[0]}</span>
+              <span className="display-flex">
+                Email: {user ? user.email : "N/A"}
+              </span>
 
-              <span>Address</span>
+              <span className="display-flex">Address</span>
               <input
                 type="text"
                 name="address" // Đặt name cho input
@@ -154,7 +155,7 @@ export default function Checkout() {
                 className="content-input"
               />
 
-              <span>Phone</span>
+              <span className="display-flex">Phone</span>
               <input
                 type="text"
                 name="phone" // Đặt name cho input
@@ -171,14 +172,17 @@ export default function Checkout() {
                 <h3 style={{ marginBottom: "10px" }}>Products</h3>
                 {reversedCartItems.map((item, index) => (
                   <p style={{ fontSize: "20px", color: "coral" }} key={item.id}>
-                    {item.name} x {item.quantity} x Rs.
-                    {(function () {
-                      const priceString = item.pricesale.replace(/\./g, "");
-                      const priceNumber = parseFloat(
-                        priceString.replace(/,/g, ".")
-                      );
-                      return priceNumber.toLocaleString("vi-VN");
-                    })()}
+                    {item.name} x {item.quantity}{" "}
+                    <p>
+                      x Rs.
+                      {(function () {
+                        const priceString = item.pricesale.replace(/\./g, "");
+                        const priceNumber = parseFloat(
+                          priceString.replace(/,/g, ".")
+                        );
+                        return priceNumber.toLocaleString("vi-VN");
+                      })()}
+                    </p>
                   </p>
                 ))}
                 <div className="container-right__display-flex2">
